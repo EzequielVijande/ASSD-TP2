68d42f72bd739c02ed0408f31cb0008d18d3ff89
EzequielVijande <evijande@itba.edu.ar>
2019-04-19 16:38:44 -0300

    Hice una funcion que separa la percusion de lo armonico

--- Ej6(sample_synthesis)/Sample_based_synthesis/main.py
+++ Ej6(sample_synthesis)/Sample_based_synthesis/main.py
@@ -4,13 +4,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 import OLA as o
 import PhaseVocoder as ph
+import SpectrumSeparator as spectre
 def MakeWindow(length,type='Hann'):
     if(type== 'Hann'):
         return np.hanning(length)
     else:
         return np.zeros(1)
 
-f_s, data= wavfile.read(".\Audio\jazz.wav")
+f_s, data= wavfile.read(".\Audio\pop.wav")
 if(len(data.shape) > 1):
     data = data[:,0]
 N=2210
@@ -18,17 +19,31 @@ window = MakeWindow(N)
 print("fs = ",f_s)
 t=np.linspace(0,len(data),num=len(data))
 tau=3*t
-#y= ph.PhVocoder(data,window,tau,int(N/2))
-y= o.OLA(data,window,tau,0.1)
-y = y.astype('int16')
-t= t/f_s
+t_h, harm, t_p, perc = spectre.GetPercussiveAndHarmonicSpectrum(data,10,10,2210,beta=2)
+harm = harm.astype('int16')
+perc = perc.astype('int16')
+
 ax1=plt.subplot(2, 1, 1)
-plt.plot(t,data)
+plt.plot(t_h,harm)
 
 ax2=plt.subplot(2, 1, 2)
-t_out= np.linspace(0,len(y),num=len(y))/f_s
-plt.plot(t_out,y)
+plt.plot(t_p,perc)
 plt.show()
-wavfile.write("Holis.wav", f_s, y)
-wavfile.write("original.wav", f_s, data)
+
+wavfile.write("Armonico.wav", f_s, harm)
+wavfile.write("Percusion.wav", f_s, perc)
+
+#y= ph.PhVocoder(data,window,tau,int(N/2))
+#y= o.OLA(data,window,tau,0.1)
+#y = y.astype('int16')
+#t= t/f_s
+#ax1=plt.subplot(2, 1, 1)
+#plt.plot(t,data)
+
+#ax2=plt.subplot(2, 1, 2)
+#t_out= np.linspace(0,len(y),num=len(y))/f_s
+#plt.plot(t_out,y)
+#plt.show()
+#wavfile.write("Holis.wav", f_s, y)
+#wavfile.write("original.wav", f_s, data)
 
