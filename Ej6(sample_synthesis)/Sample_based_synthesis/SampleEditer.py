297ad834d8935a2e0d7b1ed71da192858cef83e1
EzequielVijande <evijande@itba.edu.ar>
2019-04-20 17:22:29 -0300

    Ya empece a hacer la funcion que crea notas

--- Ej6(sample_synthesis)/Sample_based_synthesis/SampleEditer.py
+++ Ej6(sample_synthesis)/Sample_based_synthesis/SampleEditer.py
@@ -2,5 +2,30 @@ import wave
 from scipy.io import wavfile
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 
 
+f_s, data= wavfile.read(".\Samples\Guitar\guitar_Gs5_very-long_forte_normal.wav")
+if(len(data.shape) > 1):
+    data = data[:,0]
+
+t=np.linspace(0,len(data),num=len(data))
+tau=0.8*t
+t= t/f_s
+
+ax1=plt.subplot(2, 1, 1)
+plt.plot(t,data)
+plt.ylabel("Original")
+
+ax2=plt.subplot(2, 1, 2)
+
+sec_index = int(2.2*f_s)
+output = np.zeros(sec_index)
+output = data[:sec_index]
+t_out=np.linspace(0,len(output),num=len(output))/f_s
+
+plt.plot(t_out,output)
+plt.ylabel("Modificada")
+plt.show()
+
+wavfile.write("Gs5_forte_trimmed.wav", f_s, output)
--- Ej6(sample_synthesis)/Sample_based_synthesis/main.py
+++ Ej6(sample_synthesis)/Sample_based_synthesis/main.py
@@ -2,37 +2,18 @@ import wave
 from scipy.io import wavfile
 import numpy as np
 from matplotlib import pyplot as plt
-import OLA as o
-import PhaseVocoder as ph
-import SpectrumSeparator as spectre
-def MakeWindow(length,type='Hann'):
-    if(type== 'Hann'):
-        return np.hanning(length)
-    else:
-        return np.zeros(1)
+import SampleSynthesizer as sam
+import midi
 
-f_s, data= wavfile.read(".\Audio\jazz.wav")
-if(len(data.shape) > 1):
-    data = data[:,0]
-N=2210
-window = MakeWindow(N)
-print("fs = ",f_s)
-t=np.linspace(0,len(data),num=len(data))
-tau=0.8*t
-t_h, harm, t_p, perc = spectre.GetPercussiveAndHarmonicSpectrum(data,10,10,2210,beta=2)
 
-y_h= ph.PhVocoder(harm,window,tau,int(0.1*N))
-y_p= o.OLA(perc,window,tau,0.1)
-y= y_h+y_p
-y = y.astype('int16')
-t= t/f_s
-ax1=plt.subplot(2, 1, 1)
-plt.plot(t,data)
-
-ax2=plt.subplot(2, 1, 2)
-t_out= np.linspace(0,len(y),num=len(y))/f_s
-plt.plot(t_out,y)
+f_s= int(50e3)
+duration= 2
+velocity = 80
+note = sam.MakeNote(midi.C_3,duration,velocity,f_s)
+note = note.astype('int16')
+ax1=plt.subplot(1, 1, 1)
+t = np.linspace(0,duration,note.size)
+plt.plot(t,note)
 plt.show()
-wavfile.write("timeScaled.wav", f_s, y)
-wavfile.write("original.wav", f_s, data)
+wavfile.write("nota.wav", f_s, note)
 
