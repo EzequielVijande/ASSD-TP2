9ed794174ee3887cbfc0c95859effc95c0095a8b
taomasgonzalez <togonzalez@itba.edu.ar>
2019-04-21 03:22:39 -0300

    Empece con el sintetizador generico, probelam en descripcion
    Surgio un problema bastante grande que es que por lo general no se reciben eventos de cambio de tempo y el tempo inicial no se cual es... creo q se asume 120 bpm pero no se sabe...
    Tampoco se si habra otro evento de tipo tempo que no este sabiendo reconocer la libreria

--- Ejercicio 4/Código/Fm_synth.py
+++ Ejercicio 4/Código/Fm_synth.py
@@ -1,8 +1,8 @@
 import midi
-import wave
+import wav_gen
 
 
-class FmSynthesizer:
+class Synthesizer:
 
     def __init__(self):
         self.evs_dict = dict()
@@ -28,46 +28,76 @@ class FmSynthesizer:
         self.evs_dict['MIDI Port/Cable'] = None
         self.evs_dict['Track Loop'] = None
         self.evs_dict['End of Track'] = self.handle_eot
-        self.evs_dict['Set Tempo'] = None
+        self.evs_dict['Set Tempo'] = self.handle_set_tempo
         self.evs_dict['SMPTE Offset'] = None
         self.evs_dict['Time Signature'] = None
         self.evs_dict['Key Signature'] = None
         self.evs_dict['Sequencer Specific'] = None
 
+        self.on_notes = []
+        self.curr_bpm = 120
+        self.curr_resolution = -1
         self.x_out = []
+        self.last_ev_time = 0
+        self.play_note_callback = None
+
+    def set_resolution(self, resolution):
+        """set_resolution should be called every time the pattern to be synthesized is changed!"""
+        self.curr_resolution = resolution
+
+    def set_play_note_callback(self, callback):
+        self.play_note_callback = callback
 
     def synthesize(self, track: midi.Track):
-        self.x_out = self.x_out[:]
+        self.x_out = []
         for ev in track:
+            microsegs_per_tick = 60 * 10 ** 6 / self.curr_bpm / self.curr_resolution
+            self.last_ev_time += ev.tick * microsegs_per_tick
             if self.evs_dict[ev.name] is not None:
                 self.evs_dict[ev.name](ev)
 
-        self.generate_fm_wav()
+        wav_gen.generate_wav(self.x_out)
 
     def handle_note_on(self, ev: midi.NoteOnEvent):
-        pitch = ev.get_pitch()
-        vel = ev.get_velocity()
-        print('handled note on')
+        if ev.get_velocity() == 0:
+            self.off_note(ev)
+        else:
+            self.on_notes.append((ev, self.last_ev_time))
 
     def handle_note_off(self, ev: midi.NoteOffEvent):
-
-        print('handled note off')
+        self.off_note(ev)
 
     def handle_eot(self, ev: midi.EndOfTrackEvent):
-        print('handled eot')
-
-    def generate_fm_wav(self):
+        # off all notes, then end track
+        # print('handled eot')
         pass
 
-    def generate_fm_wav(self):
+    def handle_set_tempo(self, ev: midi.SetTempoEvent):
+        self.curr_bpm = ev.bpm
+
+    def off_note(self, off_ev: midi.Event):
+        """off_ev is not necessarily a NoteOffEvent
+It may also be a NoteOnEvent, hence the generic 'Event' annotation"""
+        for on_ev, ex_time in self.on_notes:
+            if on_ev.get_pitch() == off_ev.get_pitch():
+                self.play_note_callback((on_ev, ex_time), (off_ev, self.last_ev_time))
 
 
-        pass
+class FmSynthesizer(Synthesizer):
+    def __init__(self):
+        self.set_play_note_callback(self.play_note)
+        super(FmSynthesizer, self).__init__()
+
+    def play_note(self, on_tuple, off_tuple):
+        on_ev, on_time = on_tuple
+        off_ev, off_time = off_tuple
+        print('note_played: ' + str(on_ev.get_pitch) + '.\n Played from ' + str(on_time) + 'microseg to ' + str(off_time) + ' microseg.')
+
 
 # pruebas
 synth = FmSynthesizer()
+synth.set_play_note_callback(synth.play_note)
 pattern = midi.read_midifile(".\ArchivosMIDI\Super Mario 64 - Bob-Omb Battlefield.mid")
+synth.set_resolution(pattern.resolution)
 for trk in pattern:
     synth.synthesize(trk)
-
-
--- Ejercicio 4/Código/wav_gen.py
+++ Ejercicio 4/Código/wav_gen.py
@@ -1,28 +1,30 @@
 import wave
-from scipy.io import wavfile
 import math
-import numpy as np
 import struct
-
 
 # https://soledadpenades.com/posts/2009/fastest-way-to-generate-wav-files-in-python-using-the-wave-module/
 def generate_wav(data: list, n_channels: int = 1, sample_width=2, frame_rate=44100, file_name='NEW_WAV.wav'):
+    """generate_wav generates a new .wav file based on the input data.
+The amplitude of each data sample should be a float belonging to the interval [-1,1],
+where 1 will be the maximum representable number on the wav file."""
+
     new_wav_file = wave.open(file_name, 'wb')
     new_wav_file.setparams((n_channels, sample_width, frame_rate, len(data), 'NONE', 'not compressed'))
     translated_data = []
 
+    # https://docs.python.org/2/library/struct.html#struct-format-strings
+    # h : 2 bytes -> 2^15-1 =32767, maximum amplitude
+    # i : 4 bytes
     for d in data:
-        translated_data.append(struct.pack('h', int(d)))
+        translated_data.append(struct.pack('h', int(d*(2**15-1))))
 
     new_wav_file.writeframes(b''.join(translated_data))
 
 
-amp = 20000.0
-
 my_data = []
-f_rate = 44100
+f_rate = 44100          # standard frame rate for audio
 for i in range(f_rate*5):
-    my_data.append(math.cos(2*math.pi*400*i/f_rate)*amp/2)
+    my_data.append(math.cos(2*math.pi*400*i/f_rate))
 
 generate_wav(my_data, n_channels=1, sample_width=2, frame_rate=f_rate, file_name='KEASE.wav')
 
