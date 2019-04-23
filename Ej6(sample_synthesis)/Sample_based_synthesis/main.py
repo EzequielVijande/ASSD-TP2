adb8d6d65f7292d3b1e2208dccae523d01e9518d
EzequielVijande <evijande@itba.edu.ar>
2019-04-23 11:06:08 -0300

    Acorte las muestras

--- Ej6(sample_synthesis)/Sample_based_synthesis/main.py
+++ Ej6(sample_synthesis)/Sample_based_synthesis/main.py
@@ -9,9 +9,9 @@ import midi
 synthesizer = sam.SampleSynthesizer()
 synthesizer.SetInstrument('guitar')
 
-pattern = midi.read_midifile(".\ArchivosMIDI\Super Mario 64 - Bob-Omb Battlefield.mid")
+pattern = midi.read_midifile(".\ArchivosMIDI\Aguado_12valses_Op1_No1.mid")
 synthesizer.set_resolution(pattern.resolution)
 # for trk in pattern:
 #   synth.synthesize(trk)
-synthesizer.synthesize(pattern[1])
+synthesizer.synthesize(pattern[0])
 
