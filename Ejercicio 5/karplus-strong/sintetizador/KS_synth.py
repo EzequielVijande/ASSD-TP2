25448d1538781ea5dc991aee6c646ba77e54a381
Lu <lu.gfernandez@gmail.com>
2019-04-27 20:17:48 -0300

    nada

--- Ejercicio 5/karplus-strong/sintetizador/KS_synth.py
+++ Ejercicio 5/karplus-strong/sintetizador/KS_synth.py
@@ -12,27 +12,22 @@ class KSSynthesizer(synth.Synthesizer):
     def __init__(self, resolution):
         self.set_create_notes_callback(self.create_note_array)
         super(KSSynthesizer, self).__init__(resolution)
+        self.curr_instrument = ""
 
     # https://en.wikipedia.org/wiki/MIDI_tuning_standard#Frequency_values
-    def create_note_array(self, pitch, amount_of_ns: int, velocity, instrument: int):
+    def create_note_array(self, pitch, duration: int, intensity, instrument = int):
         # if instrument==drum:
-        notes = self.drum(pitch, amount_of_ns, velocity)
-        #plt.plot(notes)
-        #plt.show()
-        return notes
-        #     return notes
-        #notes = self.drum(pitch, amount_of_ns, velocity)
-        #return notes
+        return self.drum(pitch, duration, intensity)
 
 
     #cuerdas sin decay stretching
-    def stringWODecay(self, pitch, amount_of_ns: int, velocity):
+    def stringWODecay(self, pitch, duration: int, intensity):
         b = -1
         wavetable = self.create_noise(self.frame_rate, pitch)
         notes = []
         current_note = 0
         previous_note = np.float(0)
-        while len(notes) < amount_of_ns:
+        while len(notes) < duration:
             wavetable[current_note] = b * 0.5 * (wavetable[current_note] + previous_note)  # promedio
             notes.append(wavetable[current_note])
             previous_value = notes[-1]  # ultimo elemento de la lista
@@ -41,14 +36,14 @@ class KSSynthesizer(synth.Synthesizer):
         return np.array(notes)
 
 
-
-    def string(self, pitch, amount_of_ns: int, velocity):
+    def string(self, pitch, duration: int, intensity):
         wavetable = self.create_noise(self.frame_rate, pitch)
         notes = []
         current_note = 0
         previous_note = np.float(0)
-        while len(notes) < amount_of_ns:
-            rand = np.random.binomial(1, 1 - 1 / (1+velocity/127))  # sale 1 con prob=1-1/s;; sale 0 con prob 1/s (sumo 1 al s para que sea > 1)
+        while len(notes) < duration:
+            rand = np.random.binomial(1, 1 - 1 / (1 + intensity / 127))  # sale 1 con prob=1-1/s;; sale 0 con prob 1/s (sumo 1 al s para que sea > 1)
+            #rand = np.random.binomial(1, 1 - 1 / (wavetable.size*(1+intensity/127)))  # sale 1 con prob=1-1/s;; sale 0 con prob 1/s (sumo 1 al s para que sea > 1)
             if rand == 0:
                 wavetable[current_note] = 0.996 * 0.5 * (wavetable[current_note] + previous_note)  # promedio
             notes.append(wavetable[current_note])  # el promedio (con prob 1/s) o queda igual (con prob 1-1/s)
@@ -57,14 +52,15 @@ class KSSynthesizer(synth.Synthesizer):
             current_note = current_note % wavetable.size
         return np.array(notes)
 
+
     #percusion sin decay stretching
-    def drumWODecay(self, pitch, amount_of_ns: int, velocity):
+    def drumWODecay(self, pitch, duration: int, intensity):
         b = 0.5  #drumlike sound
         wavetable = np.ones(self.frame_rate // pitch)
         notes = []
         current_note = 0
         previous_note = np.float(0)
-        while len(notes) < amount_of_ns:
+        while len(notes) < duration:
             rand = np.random.binomial(1, b)  # sale 1 ó 0
             sign = 2 * rand - 1  # si sale 1 queda 1; si sale 0, queda -1
             wavetable[current_note] = sign * 0.9 * 0.5 * (wavetable[current_note] + previous_note)  # promedio
@@ -75,14 +71,15 @@ class KSSynthesizer(synth.Synthesizer):
         return np.array(notes)
 
 
-    def drum(self, pitch, amount_of_ns: int, velocity):
+    def drum(self, pitch, duration: int, intensity):
         b = 0.5  #drumlike sound
         wavetable = np.ones(self.frame_rate // pitch)
         notes = []
         current_note = 0
         previous_note = np.float(0)
-        while len(notes) < amount_of_ns:
-            rand = np.random.binomial(1, 1 - 1 / (1+velocity/127))  # sale 1 con prob=1-1/s;; sale 0 con prob 1/s (sumo 1 al s para que sea > 1)
+        while len(notes) < duration:
+            rand = np.random.binomial(1, 1 - 1 / (1 + intensity / 127))
+            #rand = np.random.binomial(1, 1 - 1 / (wavetable.size * (1+intensity/127)))  # sale 1 con prob=1-1/s;; sale 0 con prob 1/s (sumo 1 al s para que sea > 1)
             sign = -1 + 2 * np.random.binomial(1, b)   # si sale 1 queda 1; si sale 0, queda -1
             if rand == 0:
                 wavetable[current_note] = sign * 0.9 * 0.5 * (wavetable[current_note] + previous_note)  # promedio
@@ -93,6 +90,7 @@ class KSSynthesizer(synth.Synthesizer):
             current_note += 1
             current_note = current_note % wavetable.size
         return np.array(notes)
+
 
     def create_noise(self, fs, pitch):  # vector con 1 ó -1
         freq = 2 ** ((pitch - 69) / 12) * 440
